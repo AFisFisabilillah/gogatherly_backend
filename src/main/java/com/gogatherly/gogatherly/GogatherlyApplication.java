ae4accb525a4e2c93263b22711efa386527b04f0
unknown <afisfisabilillah21@gmail.com>
2025-05-19 08:39:59 +0700

    init project

---
+++ src/main/java/com/gogatherly/gogatherly/GogatherlyApplication.java
@@ -0,0 +1,13 @@
+package com.gogatherly.gogatherly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogatherlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogatherlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gogatherly/gogatherly/GogatherlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogatherly.gogatherly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogatherlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
